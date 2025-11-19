f68de5ec701de92abd1b4b1314158f999710955d
Sucheendradas V R <sucheendradas@gmail.com>
2025-11-19 19:58:16 +0530

    Update OpenAI API usage and add agent runner example
     Added agent_runner_trace.py as an example for running an agent with tracing.

--- 01_workflow/openaI/openai_api.py
+++ 01_workflow/openaI/openai_api.py
@@ -6,10 +6,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()  # Loads the .env file
 from openai import OpenAI
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY_ZOHO"])
+
+#o3-mini
+#gpt-4.1-nano
 
 response = client.responses.create(
-    model="gpt-5-mini",
+    model="gpt-4.1-nano",
     input="2+2=?"
 )
 
---
+++ openai_ai_platform/agent_runner_trace.py
@@ -0,0 +1,31 @@
+
+import os
+import asyncio
+from dotenv import load_dotenv
+
+
+from openai import OpenAI
+from agents import Agent, Runner, trace
+
+load_dotenv(override=True)  # Loads the .env file
+
+# Make an agent with name, instructions, model
+
+async def main():
+    agent = Agent(name="Jokester", instructions="You are a joke teller", model="gpt-4.1-nano")
+
+    # Run the joke with Runner.run(agent, prompt) then print final_output   
+
+    # workflow - "Telling a joke"
+    # model       - "gpt-4.1-nano"
+    # flow     - "jokester"
+    # instructions- "You are a joke teller" (System Instructions)
+    # input (user)   - "Tell a joke about Autonomous AI Agents"
+    # output (final) - result.final_output
+
+
+    with trace("Telling a joke"):
+        result = await Runner.run(agent, "Tell a joke about Autonomous AI Agents")
+        print(result.final_output)
+
+asyncio.run(main())
